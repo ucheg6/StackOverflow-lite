c140a8655eee3bba08a500d850b72fb9c38b1a65
ucheg6 <akogwuuche@ymail.com>
2018-08-17 22:07:49 +0100

    feature(api-get-question)

--- server/test/question-test.js
+++ server/test/question-test.js
@@ -27,3 +27,33 @@ describe('function getAllQuestions of Question', () => {
       });
   });
 });
+
+describe('function getQuestion of Question', () => {
+  it('should return status code 404 if question does not exist', (done) => {
+    chai.request(app)
+      .get('/api/v1/questions/10')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res).to.have.status(404);
+        expect(res.body).to.deep.equals({
+          status: 'error',
+          message: 'Question does not exist',
+        });
+        done();
+      });
+  });
+
+  it('should return status code 200 if question exists', (done) => {
+    chai.request(app)
+      .get('/api/v1/questions/4')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('matchedQuestion');
+        expect(res.body.status).to.deep.equals('success');
+        expect(res.body.matchedQuestion.question).to.deep.equals('What are Andela\'s core values?');
+        expect(res.body.message).to.deep.equal('Question found successfully');
+        done();
+      });
+  });
+});
