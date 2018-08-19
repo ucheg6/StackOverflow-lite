4c5a1170f25ff35562e4bf235c8467c8e1b151e0
ucheg6 <akogwuuche@ymail.com>
2018-08-19 12:17:28 +0100

    bugFix(handled typo in questions model)

--- server/models/questions.js
+++ server/models/questions.js
@@ -7,7 +7,7 @@ const questions = [
     },
     {
       id: 2,
-      user: 4,
+      userId: 4,
       question: 'Do you need any experience to get into Andela\'s fellowship?',
       dateTime: new Date(2018, 8, 6),
   
@@ -15,21 +15,21 @@ const questions = [
     
     {
       id: 3,
-      user: 3,
+      userId: 3,
       question: 'What are the fundamentals of web development?',
       dateTime: new Date(2018, 8, 7),
   
     },
     {
       id: 4,
-      user: 2,
+      userId: 2,
       question: 'What are Andela\'s core values?',
       dateTime: new Date(2018, 8, 8),
   
     },
     {
       id: 5,
-      user: 1,
+      userId: 1,
       question: 'Who is Andela\'s CEO?',
       dateTime: new Date(2018, 8, 19),
   
