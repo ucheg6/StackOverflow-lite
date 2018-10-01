7a0625e6879de4a2c9492bc0b8ce5ed4d1e06758
ucheg6 <akogwuuche@ymail.com>
2018-10-01 20:44:06 +0100

    ft(consume api endpoint): consume api endpoint get most answers starts#160899777

--- UI/fetchScripts/getQuestions.js
+++ UI/fetchScripts/getQuestions.js
@@ -14,7 +14,6 @@ window.onload = () => {
        
         const { questionid, userid, fullname, questiontopic, questionbody, created_at } = questions
        
-        console.log(questions)
         let result =
           ` <div class="feed">
         <div class="feed-item">
@@ -58,4 +57,42 @@ window.onload = () => {
     }).catch((error) => {
       console.log(error);
     });
-}                
+
+
+    fetch('https://stackoverflow-litee.herokuapp.com/api/v1//questions/mostanswers', options)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data)
+        data.data.map((questions) => {
+       
+        const { questionid, answersnumber, questionbody, created_at } = questions
+       
+        console.log(questions)
+        let result =
+          `  <div class="feed-item" id="mostAnswers">
+          
+          <div class="post-body">
+            <div class="article-title">
+              ${questionbody}
+            </div>
+
+          </div>
+          <div class="feed-item-actions">
+            <ul class="action-buttons">
+
+              <li class="button" id="button-up">
+                <a href="question.html">
+                  <i class="fa fa-plane"></i>${answersnumber} Answers </a>
+              </li>
+
+            </ul>
+          </div>
+        </div>`;
+        document.getElementById('mostAnswered').innerHTML += result;
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+             
+
+}
